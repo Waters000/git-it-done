0e8ccab01ebb5456884da884027648f4f3c182ac
Chris Waters <floridarealty001@gmail.com>
2022-01-25 11:06:43 -0500

    URL direct handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,64 +1,87 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+// get query string to a variable
 
-var getRepoIssues = function(repo) {
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo given, redirect back to the homepage.
+        document.location.replace(".index.html");
+    }
+
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+    console.log(repoName);
+};
+
+
+var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl).then(function(response){
+    fetch(apiUrl).then(function (response) {
         // request was successful
         if (response.ok) {
-            response.json().then(function(data){
+            response.json().then(function (data) {
                 // pass reponse data to dom function
                 displayIssues(data);
                 // check if api has pagainated issues//more than 30 issues
-                if (response.headers.get("Link")){
+                if (response.headers.get("Link")) {
                     displayWarning(repo)
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            // if not successful, direct back to homepage
+            document.location.replace(".index.html");
         }
     });
 };
 
 
-var displayIssues = function(issues) {
-// check for issues 
-if (issues.length === 0) {
-    issueContainerEl.textContent = "This repo has no open issues!";
-    return;
-}
-
-for (var i = 0; i < issues.length; i++) {
-    // create a link element to take users to the issue on github
-    var issueEl = document.createElement("a");
-    issueEl.classList = "list-item flex-row justify-space-between align center";
-    issueEl.setAttribute("href", issues[i].html_url);
-    issueEl.setAttribute("target", "_blank");
-
-    // create span to hold issue title
-    var titleEl = document.createElement("span");
-    titleEl.textContent = issues[i].title;
-
-    // append to container
-    issueEl.appendChild(titleEl);
-
-    // create a type element
-    var typeEl = document.createElement("span");
-
-    // check if issue is an actual issue or a pull request
-    if (issues[i].pull_request) {
-        typeEl.textContent = "(Pull request)";
-    } else {
-        typeEl.textContent = "(Issue)";
+var displayIssues = function (issues) {
+    // check for issues 
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
     }
-    // append to container
-    issueEl.appendChild(typeEl);
-    issueContainerEl.appendChild(issueEl);
-}
+
+    for (var i = 0; i < issues.length; i++) {
+        // create a link element to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        // create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+        // append to container
+        issueEl.appendChild(typeEl);
+        issueContainerEl.appendChild(issueEl);
+    }
 };
 
-var displayWarning = function(repo) {
+var displayWarning = function (repo) {
     // add text to warning container
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
@@ -73,4 +96,5 @@ var displayWarning = function(repo) {
 };
 
 
-getRepoIssues("facebook/react")
+
+getRepoName();
